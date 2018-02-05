2dc8bb2f75e98b7944dd68b56a41ac78ad1cc722
Niels Larmuseau <nielslarmuseau@gmail.com>
2018-02-05 11:03:28 +0100

    termsandconditions

--- source/termsandconditions.php
+++ source/termsandconditions.php
@@ -2,6 +2,23 @@
 require_once 'autoloader.php';
 output::header("termsandconditions");
 ?>
+<div class="termsandconditions">
+    <h1>Terms & Conditions</h1>
+    <section>
+        <h2>Terms and Conditions of Stephanie Jackson Limited</h2>
+        <ul>
+            <li>There is a £50 deposit charge to secure your booking. This is non refundable.</li>
+            <li>Change of booking is only allowed once, 48 hours prior to the appointment or a loss of deposit will occur.</li>
+            <li>Once your initial booking has been made and the deposit has been paid, you’ll only be able to change your appointment once. If you need to change your appointment a second time, you’ll need to re pay the deposit again for a new appointment.</li>
+            <li>If a third treatment is needed there will be a cover charge of £50 and this MUST be done within 3 months from the touch up date. (This can be due to aftercare/the individuals skin or the overall look one wants to achieve).</li>
+            <li>I am not responsible for offering a third treatment free of charge.</li>
+            <li>Please understand you must return for your touch up procedure within the 8 week period, otherwise there will be a £50 charge to cover the extra time.</li>
+            <li>The  Services are allowed to be declined if doctors documentation is not presented when required.</li>
+            <li>I do not offer the touch up treatment on a Saturday as I leave that open to new clients, due to high demand for weekend appointments.</li>
+            <li>Lastly please be prepared to heal for a minimum of 2 weeks, including no sun exposure/sunbed use, no heavy exercise, no water contact what so ever, and pealing over the treatment area will occur during this period. Vaseline or oil will be given to help the process.</li>
+        </ul>
+    </section>
+</div>
 <?php
 output::footer();
 ?>
